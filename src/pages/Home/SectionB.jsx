56817669ada44579d82fbeecb306fa248ef92289
taewonLee <xodnjs6038@naver.com>
2025-02-07 14:48:18 +0900

    Framer-motion add

--- src/pages/Home/SectionB.jsx
+++ src/pages/Home/SectionB.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const SectionB = () => {
   return (
     <section className="p-16 pb-40 relative">
-        <div className="absolute inset-0 z-0 bg-black bg-grid-pattern bg-grid-size"></div>
+      <div className="absolute inset-0 z-0 bg-black bg-grid-pattern bg-grid-size"></div>
       <div className="content z-10 relative grid gap-y-20">
         <div className="">
           <h2 className="text-white font-serif text-5xl">Section B</h2>
@@ -15,7 +16,22 @@ const SectionB = () => {
           <div className="">
             <p className="text-white font-serif text-xl">This is the content of Section B.</p>
             <div className="mt-20">
-            <span className="text-red-600">(Listing) (Marketing) (Blockchain Deal) (&gt;&gt;)</span>
+              <motion.span 
+              className="relative text-red-700 inline-block" 
+              initial={{ scale: 1 }} 
+              whileHover={{ scale: 1.2 }} 
+              transition={{ duration: 0.3 }}
+              style={{ originX: 0 }}
+              >
+                &#123;Listing&#125; &#123;Marketing&#125; &#123;Blockchain Deal&#125; &#123; &gt;&gt; &#125;
+                <motion.div
+                className="absolute left-0 bottom-0 bg-red-700 h-px w-full"
+                initial={{ scaleX: 0 }}
+                whileHover={{ scaleX: 1 }}
+                transition={{ duration: 0.3 }}
+                >
+                </motion.div>
+              </motion.span>
             </div>
           </div>
         </div>
